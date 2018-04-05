fe0443c6b44fb0e54da202f148181b6f88f37c6a
Bastian Venthur <bastian.venthur@flixbus.com>
2018-04-05 11:28:04 +0200

    make pep8 happy

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -11,19 +11,22 @@ def test_schema_default():
 
 def test_schema_v3():
     """No errors when calling proper v3 file."""
-    testargs = ['--schema',  '3.0.0', './tests/integration/data/v3.0/petstore.yaml']
+    testargs = ['--schema', '3.0.0',
+                './tests/integration/data/v3.0/petstore.yaml']
     main(testargs)
 
 
 def test_schema_v2():
     """No errors when calling with proper v2 file."""
-    testargs = ['--schema', '2.0', './tests/integration/data/v2.0/petstore.yaml']
+    testargs = ['--schema', '2.0',
+                './tests/integration/data/v2.0/petstore.yaml']
     main(testargs)
 
 
 def test_schema_unknown():
     """Errors on running with unknown schema."""
-    testargs = ['--schema', 'x.x', './tests/integration/data/v2.0/petstore.yaml']
+    testargs = ['--schema', 'x.x',
+                './tests/integration/data/v2.0/petstore.yaml']
     with pytest.raises(SystemExit):
         main(testargs)
 
