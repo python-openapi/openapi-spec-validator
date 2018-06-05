61b6839bcde37f513f523163670f9748f379e3f3
Daniel Grossmann-Kavanagh <kavanaghdaniel@gmail.com>
2018-06-04 21:28:58 -0700

    drop python3.3 support (EOL)

--- setup.py
+++ setup.py
@@ -86,7 +86,6 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
