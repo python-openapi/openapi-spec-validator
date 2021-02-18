20ca8cd2861d03c39c70cf25c1a04ef689c90db6
p1c2u <maciag.artur@gmail.com>
2021-02-18 19:12:44 +0000

    Handlers timeout setup fix

--- openapi_spec_validator/handlers/requests.py
+++ openapi_spec_validator/handlers/requests.py
@@ -13,17 +13,18 @@ class UrlRequestsHandler(FileHandler):
     """OpenAPI spec validator URL (requests) scheme handler."""
 
     def __init__(self, *allowed_schemes, **options):
+        self.timeout = options.pop('timeout', 10)
         super(UrlRequestsHandler, self).__init__(**options)
         self.allowed_schemes = allowed_schemes
 
-    def __call__(self, url, timeout=1):
+    def __call__(self, url):
         scheme = urlparse(url).scheme
         assert scheme in self.allowed_schemes
 
         if scheme == "file":
             return super(UrlRequestsHandler, self).__call__(url)
 
-        response = requests.get(url, timeout=timeout)
+        response = requests.get(url, timeout=self.timeout)
         response.raise_for_status()
 
         data = StringIO(response.text)
--- openapi_spec_validator/handlers/urllib.py
+++ openapi_spec_validator/handlers/urllib.py
@@ -11,13 +11,14 @@ class UrllibHandler(FileObjectHandler):
     """OpenAPI spec validator URL (urllib) scheme handler."""
 
     def __init__(self, *allowed_schemes, **options):
+        self.timeout = options.pop('timeout', 10)
         super(UrllibHandler, self).__init__(**options)
         self.allowed_schemes = allowed_schemes
 
-    def __call__(self, url, timeout=1):
+    def __call__(self, url):
         assert urlparse(url).scheme in self.allowed_schemes
 
-        f = urlopen(url, timeout=timeout)
+        f = urlopen(url, timeout=self.timeout)
 
         with contextlib.closing(f) as fh:
             return super(UrllibHandler, self).__call__(fh)
