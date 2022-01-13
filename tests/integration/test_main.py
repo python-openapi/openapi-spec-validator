70b54825d54229806d8c896d42085999c1cc86a0
p1c2u <maciag.artur@gmail.com>
2022-01-13 01:42:04 +0000

    Switch to poetry

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -1,9 +1,12 @@
-import mock
-
 import pytest
 from six import StringIO
 
 from openapi_spec_validator.__main__ import main
+
+try:
+    from unittest import mock
+except ImportError:
+    import mock
 
 
 def test_schema_default():
