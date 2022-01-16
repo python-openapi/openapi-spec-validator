27408847b25ad80c651cccda918314d02f8d22ca
p1c2u <maciag.artur@gmail.com>
2022-01-16 04:57:51 +0000

    Version 0.3.2

--- openapi_spec_validator/__init__.py
+++ openapi_spec_validator/__init__.py
@@ -9,7 +9,7 @@ from openapi_spec_validator.validators import SpecValidator
 
 __author__ = 'Artur Maciag'
 __email__ = 'maciag.artur@gmail.com'
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 __url__ = 'https://github.com/p1c2u/openapi-spec-validator'
 __license__ = 'Apache License, Version 2.0'
 
